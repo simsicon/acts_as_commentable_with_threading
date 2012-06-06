dbd9723effa01a976075c973878653fc9db65b5a
simsicon <simsicon@gmail.com>
2012-06-06 13:41:05 +0800

    customize user to person

--- lib/generators/acts_as_commentable_upgrade_migration/comment.rb
+++ lib/generators/acts_as_commentable_upgrade_migration/comment.rb
@@ -11,7 +11,7 @@ class Comment < ActiveRecord::Base
   belongs_to :commentable, :polymorphic => true
 
   # NOTE: Comments belong to a user
-  belongs_to :user
+  belongs_to :user, :class_name => "Person", :foreign_key => "user_id"
 
   # Helper class method that allows you to build a comment
   # by passing a commentable object, a user_id, and comment text
